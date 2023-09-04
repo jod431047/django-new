d016996d73d0981220a5c6f850ccb5b0473a160e
jod431047 <jod431047@gmail.com>
2023-09-04 16:36:57 +0200

    add company model

--- settings/models.py
+++ settings/models.py
@@ -5,5 +5,16 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=50)
     logo = models.ImageField(upload_to='company')
-    call_us = models.CharField(max_length=25)
-    email_us = models.CharField(max_length=25)
+    call_us = models.CharField(max_length=25,null=True,blank=True)
+    email_us = models.CharField(max_length=25,null=True,blank=True)
+    subtitle = models.TextField(max_length=200,null=True,blank=True)
+    email = models.TextField(max_length=200,null=True,blank=True)
+    phones = models.TextField(max_length=200,null=True,blank=True)
+    address = models.TextField(max_length=200,null=True,blank=True)
+    fb_link = models.URLField(null=True,blank=True)
+    youtube_link = models.URLField(null=True,blank=True)
+    insta_link = models.URLField(null=True,blank=True)
+    twiter_link = models.URLField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
