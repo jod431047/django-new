f5f8484b489cbe8c7afaeab90ff1c5e668e3f0a6
jod431047 <jod431047@gmail.com>
2023-09-04 14:43:32 +0200

    add dummy products

--- dummy_data.py
+++ dummy_data.py
@@ -1,3 +1,7 @@
+import os , django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
 
 from faker import Faker
 import random
@@ -5,9 +9,17 @@ from products.models import Products
 
 
 def seed_product(n):
-    pass
+    fake = Faker()
+    images = ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png']
+    
+    for x in range(n):
+        Products.objects.create(
+            name = fake.name(),
+            price = round(random.uniform(200.99 , 999.99),2),
+            image = f'products/{images[random.randint(0,5)]}'
+        )
+    print(f'{n} products was successfuly')
 
 
 
-
-seed_product(20)
+seed_product(75)
