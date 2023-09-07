c4046f40bfd1108a06a919a40cf5b5a154df56a3
jod431047 <jod431047@gmail.com>
2023-09-07 22:25:18 +0200

    design user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+#from utils.generate_code import generate_code
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,related_name='user_profile', on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='accounts', blank=True, null=True)
+    address = models.CharField(max_length=200)
+    #code = models.CharField(max_length=10,default=generate_code)
+    # Add other fields you want in the user profile
+
+    def __str__(self):
+        return str(self.user)
