a15aa3f780de9c89e19f0490cf92310c72d40637
jod431047 <jod431047@gmail.com>
2023-09-07 22:33:57 +0200

    add utils in project

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
-#from utils.generate_code import generate_code
+from utils.generate_code import generate_code
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,related_name='user_profile', on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='accounts', blank=True, null=True)
     address = models.CharField(max_length=200)
-    #code = models.CharField(max_length=10,default=generate_code)
+    code = models.CharField(max_length=10,default=generate_code)
     # Add other fields you want in the user profile
 
     def __str__(self):
